70fd200d33e13c50bffaef1fa3aea2cc4823b758
radeveloper <radeveloper@gmail.com>
2021-01-31 22:34:05 +0300

    Discover Companies Slide impl.

--- js/custom.js
+++ js/custom.js
@@ -1,70 +1,20 @@
 $(document).ready(function () {
     const jobPostings = $('.job-postings');
-    jobPostings.owlCarousel({
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            }
-        }
-    });
-    $('.jobPostingsNext').click(function () {
-        jobPostings.trigger('next.owl.carousel');
-    })
-    $('.jobPostingsPrev').click(function () {
-        jobPostings.trigger('prev.owl.carousel', [300]);
-    })
-
-
+    jobPostings.owlCarousel({responsiveClass: true, responsive: {0: {items: 1,}, 600: {items: 2,}, 1000: {items: 3,}}});
+    $('.jobPostingsNext').click(function () {jobPostings.trigger('next.owl.carousel');});
+    $('.jobPostingsPrev').click(function () {jobPostings.trigger('prev.owl.carousel', [300]);});
     const careerTools = $('.career-tools');
-    careerTools.owlCarousel({
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            }
-        }
-    });
-    $('.careerToolsNext').click(function () {
-        careerTools.trigger('next.owl.carousel');
-    })
-    $('.careerToolsPrev').click(function () {
-        careerTools.trigger('prev.owl.carousel', [300]);
-    })
-
-
+    careerTools.owlCarousel({responsiveClass: true, responsive: {0: {items: 1,}, 600: {items: 2,}, 1000: {items: 3,}}});
+    $('.careerToolsNext').click(function () {careerTools.trigger('next.owl.carousel');});
+    $('.careerToolsPrev').click(function () {careerTools.trigger('prev.owl.carousel', [300]);});
     const recruitmentPrograms = $('.recruitment-programs');
-    recruitmentPrograms.owlCarousel({
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            }
-        }
-    });
-    $('.recruitmentProgramsNext').click(function () {
-        recruitmentPrograms.trigger('next.owl.carousel');
-    })
-    $('.recruitmentProgramsPrev').click(function () {
-        recruitmentPrograms.trigger('prev.owl.carousel', [300]);
-    })
+    recruitmentPrograms.owlCarousel({responsiveClass: true, responsive: {0: {items: 1,}, 600: {items: 2,}, 1000: {items: 3,}}});
+    $('.recruitmentProgramsNext').click(function () {recruitmentPrograms.trigger('next.owl.carousel');});
+    $('.recruitmentProgramsPrev').click(function () {recruitmentPrograms.trigger('prev.owl.carousel', [300]);});
+    const discoverCompanies = $('.discover-companies');
+    discoverCompanies.owlCarousel({responsiveClass: true, responsive: {0: {items: 1,}, 600: {items: 2,}, 1000: {items: 3,}}});
+    $('.discoverCompaniesNext').click(function () {discoverCompanies.trigger('next.owl.carousel');});
+    $('.discoverCompaniesPrev').click(function () {discoverCompanies.trigger('prev.owl.carousel', [300]);});
 
 });
+
